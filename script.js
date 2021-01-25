63b0784f5451607d1245777600e2955e985949aa
Fran Carre <francarre19@gmail.com>
2021-01-25 23:56:54 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const pageviewsEl = document.getElementById('pageviews')
+const periodEl    = document.getElementById('period')
+const priceEl     = document.getElementById('price')
+const rangeEl     = document.getElementById('range')
