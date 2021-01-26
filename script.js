5609d2fe22c15f3abed09bdafb56cdce06e01f03
Fran Carre <francarre19@gmail.com>
2021-01-26 18:14:46 +0500

    Add slider script

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 const pageviewsEl = document.getElementById('pageviews')
-const periodEl    = document.getElementById('period')
 const priceEl     = document.getElementById('price')
-const rangeEl     = document.getElementById('range')
+const sliderEl     = document.getElementById('range')
+const toggleEl    = document.getElementById('toggle')
+
+const pricing = [
+  ['10K', 8],
+  ['50K', 12],
+  ['100K', 16],
+  ['500K', 24],
+  ['1M', 36],
+]
+
+let year = false
+let newPrice = 0
+
+
+sliderEl.oninput = () => {
+  setValues(sliderEl.value)
+}
+
+toggleEl.onclick = () => {
+  toggleEl.checked == true ? year = true : year = false
+  setValues(sliderEl.value)
+}
+
+function setValues(value) {
+  const [pageviews, price] = pricing[value]
+  if (year === true) {
+    newPrice = pricing[value][1] / 100 * 75
+    priceEl.innerText = `$${newPrice}.00`
+  } else {
+    priceEl.innerText = `$${price}.00`
+  }
+  pageviewsEl.innerText = `${pageviews} pageviews`
+
+  // slider background
+  const gradientProgress = 25 * value
+  sliderEl.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${gradientProgress}%, hsl(224, 65%, 95%) 0%, hsl(224, 65%, 95%) 100%)`
+  console.log(gradientProgress);
+}
